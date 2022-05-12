193f6366cbb3608914ed69bb6c718d86d21b27bb
brittanymy <brittany.young2001@gmail.com>
2022-05-12 12:29:12 -0400

    readme completion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,6 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 $(document).ready(function () {
-  // Save button function
-  $(".saveBtn").on("click", function () {
-    // Collects sibling and child elements from HTML
-    let text = $(this).siblings(".description").val();
-    let time = $(this).parent().attr("id");
-
-    // Saves to local storage
-    localStorage.setItem(time, text);
-  });
-
-  // Color code function
-  function timeExpression() {
-
-    let currentTime = moment().hour();
-
-    $(".time-block").each(function () {
-      let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-
-      if (timeBlock < currentTime) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-      } else if (timeBlock === currentTime) {
-        $(this).removeClass("past")
-        $(this).removeClass("future");
-        $(this).addClass("present");
-      } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
-    })
-  }
-
   // Grabs item from local storage
   $("#hour9.description").val(localStorage.getItem("hour9"));
   $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -47,5 +13,37 @@ $(document).ready(function () {
   $("#hour16 .description").val(localStorage.getItem("hour16"));
   $("#hour17 .description").val(localStorage.getItem("hour17"));
 
+  // Color code function
+  function timeExpression() {
+    let currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+      let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (timeBlock < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (timeBlock === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Save button function
+  $(".saveBtn").on("click", function () {
+    // Collects sibling and child elements from HTML
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    // Saves to local storage
+    localStorage.setItem(time, text);
+  });
   timeExpression();
 });
